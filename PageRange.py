0cea0ddb9c39a540fd57058ed3969bc3562bd444
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-12-04 02:15:08 +0100

    Initialize Mapnik for PageRange

--- PageRange.py
+++ PageRange.py
@@ -1,7 +1,32 @@
 from Page import *
+import mapnik
 
 class PageRange:
-    def __init__(self, range_config):
+    def __init__(self, common, config):
         self.data = []
-        self.range_config = range_config
-        self.pages = Page(range_config)
+        self.config = config
+        print config
+
+        # Initialize Mapnik (with an arbitrary size)
+        self.map = mapnik.Map(10000, 10000)
+        mapnik.load_map(self.map, config['map_file'])
+
+        # ensure the target map projection is mercator
+        self.map.srs = common['merc'].params()
+
+        # set up projection
+        self.map.transform = mapnik.ProjTransform(common['longlat'], common['merc'])
+
+        # Mapnik: set up bbox on correct projection and scale
+        self.bbox = mapnik.Box2d(*self.config['bounds'])
+        self.bbox = self.map.transform.forward(self.bbox)
+        self.map.zoom_to_box(self.bbox)
+
+        # Force scale denominator
+        scale_change = self.config['scale'] / self.map.scale_denominator()
+        # Resize image size
+        self.map.resize(int(10000 / scale_change + 1), int(10000 / scale_change + 1))
+        self.map.zoom(self.config['scale'] / self.map.scale_denominator())
+
+        # total map size without overlap
+        self.map_size = (self.map.width, self.map.height)
